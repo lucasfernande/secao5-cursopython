2bf0e900e3b42c139d765288e7dda30df25336d6
lucasfernande <lucas.fernandes6192@outlook.com>
2021-04-05 13:49:50 -0300

    JSON

--- AULA130_Shutil/main.py
+++ AULA130_Shutil/main.py
@@ -2,10 +2,10 @@ import os
 import shutil
 
 caminho_original = r'C:\Users\lucas\Documents\teste'
-caminho_novo =  r'C:\Users\lucas\Documents\teste2'
+caminho_novo = r'C:\Users\lucas\Documents\teste2'
 
 try:
-    os.mkdir(caminho_novo) # criando uma nova pasta
+    os.mkdir(caminho_novo)  # criando uma nova pasta
 except FileExistsError:
     print(f'Essa pasta ({caminho_novo}) já existe')
 
@@ -14,8 +14,8 @@ for root, dirs, files in os.walk(caminho_original):
         old_file_path = os.path.join(root, file)
         new_file_path = os.path.join(caminho_novo, file)
 
-        shutil.move(old_file_path, new_file_path) # movendo os arquivos da pasta teste para a teste2
+        shutil.move(old_file_path, new_file_path)  # movendo os arquivos da pasta teste para a teste2
         print(f'Arquivo {file} movido!')
 
-        shutil.copy(new_file_path, old_file_path) # copiando os arquivos de volta para a pasta teste
+        shutil.copy(new_file_path, old_file_path)  # copiando os arquivos de volta para a pasta teste
         print(f'Arquivo {file} copiado!')
