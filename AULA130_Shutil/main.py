cc95474bb82ca9b8cd23686a169974e993b17eba
lucasfernande <lucas.fernandes6192@outlook.com>
2021-04-05 09:53:23 -0300

    movendo arquivos com shutil

---
+++ AULA130_Shutil/main.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+caminho_original = r'C:\Users\lucas\Documents\teste'
+caminho_novo =  r'C:\Users\lucas\Documents\teste2'
+
+try:
+    os.mkdir(caminho_novo) # criando uma nova pasta
+except FileExistsError:
+    print(f'Essa pasta ({caminho_novo}) já existe')
+
+for root, dirs, files in os.walk(caminho_original):
+    for file in files:
+        old_file_path = os.path.join(root, file)
+        new_file_path = os.path.join(caminho_novo, file)
+
+        shutil.move(old_file_path, new_file_path) # movendo os arquivos da pasta teste para a teste2
+        print(f'Arquivo {file} movido!')
