a190cdd105abac6b6cb1dac0c4ae31356f9b786b
lucasfernande <lucas.fernandes6192@outlook.com>
2021-04-08 15:02:47 -0300

    fazendo logout

--- AULA140_Selenium/app.py
+++ AULA140_Selenium/app.py
@@ -26,14 +26,30 @@ class ChromeAuto:
         try:
             input_login = self.chrome.find_element_by_id('login_field')  # selecionando o campo email
             input_password = self.chrome.find_element_by_id('password')  # selecionando o campo senha
+            btn_sign_in = self.chrome.find_element_by_name('commit')
 
             input_login.send_keys('lucasfernandemwm@gmail.com')
             input_password.send_keys('ap3kilimei')
 
-            btn_sign_in = self.chrome.find_element_by_name('commit')
+            sleep(2)
             btn_sign_in.click()
         except Exception as e:
             print(f'Erro no login: {e}')
+
+    def clicar_perfil(self):  # função para clicar no perfil
+        try:
+            perfil = self.chrome.find_element_by_css_selector(
+                'body > div.position-relative.js-header-wrapper > header > div.Header-item.position-relative.mr-0.d-none.d-md-flex > details')
+            perfil.click()
+        except Exception as e:
+            print(f'Erro ao clicar no perfil: {e}')
+
+    def fazer_logout(self):
+        try:
+            btn_sign_out = self.chrome.find_element_by_css_selector('body > div.position-relative.js-header-wrapper > header > div.Header-item.position-relative.mr-0.d-none.d-md-flex > details > details-menu > form > button')
+            btn_sign_out.click()
+        except Exception as e:
+            print(f'Erro ao sair: {e}')
 
 
 if __name__ == '__main__':
@@ -41,6 +57,10 @@ if __name__ == '__main__':
     chrome.acessa('https://github.com/')
     chrome.clicar_entrar()
     chrome.fazer_login()
+    sleep(2)
+    chrome.clicar_perfil()
+    sleep(2)
+    chrome.fazer_logout()
 
-    sleep(5)
+    sleep(10)
     chrome.sair()
