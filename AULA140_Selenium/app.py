1dab2ffd806e8cd203d814f6a5b19ac6065265e7
lucasfernande <lucas.fernandes6192@outlook.com>
2021-04-07 21:23:03 -0300

    selenium - abrindo o chrome pelo python

---
+++ AULA140_Selenium/app.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from time import sleep
+
+
+class ChromeAuto:
+    def __init__(self):
+        self.driver_path = 'chromedriver.exe'
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument('user-data-dir=Perfil')
+        self.chrome = webdriver.Chrome(self.driver_path, options=self.options)
+
+
+if __name__ == '__main__':
+    chrome = ChromeAuto()
