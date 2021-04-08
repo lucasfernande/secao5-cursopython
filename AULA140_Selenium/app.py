91390a9eaaf1da090fdb04e7bf80a9e7228c0422
lucasfernande <lucas.fernandes6192@outlook.com>
2021-04-08 14:40:52 -0300

    fazendo login no github

--- AULA140_Selenium/app.py
+++ AULA140_Selenium/app.py
@@ -9,16 +9,38 @@ class ChromeAuto:
         self.options.add_argument('user-data-dir=Perfil')  # definindo a pasta de perfil de usuário
         self.chrome = webdriver.Chrome(self.driver_path, options=self.options)
 
-    def acessa(self, site): # função para acessar sites
+    def acessa(self, site):  # função para acessar sites
         self.chrome.get(site)
 
-    def sair(self): # função para fechar o navegador
+    def sair(self):  # função para fechar o navegador
         self.chrome.quit()
+
+    def clicar_entrar(self):  # função para clicar no botão entrar
+        try:
+            btn_sign_in = self.chrome.find_element_by_link_text('Sign in')
+            btn_sign_in.click()
+        except Exception as e:
+            print(f'Erro: {e}')
+
+    def fazer_login(self):
+        try:
+            input_login = self.chrome.find_element_by_id('login_field')  # selecionando o campo email
+            input_password = self.chrome.find_element_by_id('password')  # selecionando o campo senha
+
+            input_login.send_keys('lucasfernandemwm@gmail.com')
+            input_password.send_keys('ap3kilimei')
+
+            btn_sign_in = self.chrome.find_element_by_name('commit')
+            btn_sign_in.click()
+        except Exception as e:
+            print(f'Erro no login: {e}')
 
 
 if __name__ == '__main__':
     chrome = ChromeAuto()
-    chrome.acessa('https://g1.globo.com')
+    chrome.acessa('https://github.com/')
+    chrome.clicar_entrar()
+    chrome.fazer_login()
 
-    sleep(2)
+    sleep(5)
     chrome.sair()
