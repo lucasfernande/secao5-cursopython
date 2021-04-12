3e0a5b3d3c8e1f383a77fc8642968100fd8cd489
lucasfernande <lucas.fernandes6192@outlook.com>
2021-04-12 12:27:10 -0300

    deque - usando o maxlen

--- AULA145_Deque-LIFO-FIFO/main.py
+++ AULA145_Deque-LIFO-FIFO/main.py
@@ -15,13 +15,20 @@ modificados.
 
 # LIFO
 
+"""
+
 livros = ['Livro 1', 'Livro 2', 'Livro 3', 'Livro 4', 'Livro 5']  # o livro 5 foi o último a entrar
 livro_removido = livros.pop()
 print(livros, livro_removido)
 
+"""
+
 # FIFO
 
 from collections import deque
+from time import sleep
+
+"""
 
 fila = deque()
 fila.append('Joãozinho')  # joãozinho foi o primeiro a entrar
@@ -33,3 +40,12 @@ print(fila)
 print(f'Saiu: {fila.popleft()}')  # joãozinho vai ser o primeiro a sair
 print(f'Saiu: {fila.popleft()}')
 print(fila)
+
+"""
+
+nums = deque(maxlen=10)  # maxlen = define o máximo de itens que a lista pode ter
+
+for i in range(100):
+    nums.append(i)  # quando a lista tiver 10 itens, o primeiro item será removido
+    sleep(1)
+    print(nums)
