77fd64d55b9b0883de3717d20af6ef5bf9db1dd6
lucasfernande <lucas.fernandes6192@outlook.com>
2021-04-12 12:19:18 -0300

    deque - exemplos de lifo e fifo

---
+++ AULA145_Deque-LIFO-FIFO/main.py
@@ -0,0 +1,35 @@
+"""
+
+Pilhas e filas
+
+Pilha (stack) - LIFO - last in e first out
+    Exemplo: pilha de livros - o último a entrar é o primeiro a sair
+
+Fila (queue) - FIFO - first in e first out
+    Exemplo: fila de banco - o primeiro a entrar é o primeiro a sair
+
+Filas podem afetar o desempenho, porque a cada item alterado, todos os índices serão
+modificados.
+
+"""
+
+# LIFO
+
+livros = ['Livro 1', 'Livro 2', 'Livro 3', 'Livro 4', 'Livro 5']  # o livro 5 foi o último a entrar
+livro_removido = livros.pop()
+print(livros, livro_removido)
+
+# FIFO
+
+from collections import deque
+
+fila = deque()
+fila.append('Joãozinho')  # joãozinho foi o primeiro a entrar
+fila.append('Maria')
+fila.append('Mariana')
+fila.append('Luiz')
+print(fila)
+
+print(f'Saiu: {fila.popleft()}')  # joãozinho vai ser o primeiro a sair
+print(f'Saiu: {fila.popleft()}')
+print(fila)
